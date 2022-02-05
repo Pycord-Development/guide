3c2ef0722aced6a2c1fdbb5206db5331a669b068
Matteu <matteugt@gmail.com>
2022-02-05 23:24:02 +0800

    FIX: Add basic theme overrides

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,101 +6,97 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Pycord Guide',
-  tagline: 'Imagine a place... where you can learn how to add more features to your Pycord bot',
-  url: 'localhost',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'Pycord-Development', // Usually your GitHub org/user name.
-  projectName: 'Guide', // Usually your repo name.
-  plugins: ['docusaurus-plugin-sass'],
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/Pycord-Development/guide/tree/master/guide',
-        },
-        theme: {
-          customCss: require.resolve('./src/scss/main.scss'),
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Pycord Guide',
-        logo: {
-          alt: 'Pycord Logo SVG',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'Guide'
-          }
-        ]
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Guide',
-            items: [
-              {
-                label: 'Index',
-                to: '/docs/index.md',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/pycord',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discord.gg/pycord',
-              },
-              {
-                label: 'Github',
-                href: 'https://github.com/Pycord-Development',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Twitter',
-                to: 'https://twitter.com/PycordDev',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
+    title: 'Pycord Guide',
+    tagline: 'Imagine a place... where you can learn how to add more features to your Pycord bot',
+    url: 'localhost',
+    baseUrl: '/',
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+    favicon: 'img/favicon.ico',
+    organizationName: 'Pycord-Development', // Usually your GitHub org/user name.
+    projectName: 'Guide', // Usually your repo name.
+    plugins: ['docusaurus-plugin-sass'],
+    presets: [
+        [
+            'classic',
+            ({
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    editUrl: 'https://github.com/Pycord-Development/guide/tree/master/guide',
+                },
+                theme: {
+                    customCss: require.resolve('./src/scss/main.scss'),
+                },
+            }),
         ],
-        copyright: `Copyright © 2022-present Pycord Development, Built with ❤️ and Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+    ],
+
+    themeConfig: {
+        colorMode: {
+            defaultMode: 'dark',
+            disableSwitch: true
+        },
+        navbar: {
+            title: 'Pycord Guide',
+            items: [
+                {
+                    type: 'doc',
+                    docId: 'index',
+                    position: 'left',
+                    label: 'Guide'
+                }
+            ]
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Guide',
+                    items: [
+                        {
+                            label: 'Index',
+                            to: '/docs/index.md',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/pycord',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discord.gg/pycord',
+                        },
+                        {
+                            label: 'Github',
+                            href: 'https://github.com/Pycord-Development',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Twitter',
+                            to: 'https://twitter.com/PycordDev',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/facebook/docusaurus',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${ new Date().getFullYear() } Pycord Development`,
+        },
+        prism: {
+            theme: lightCodeTheme,
+            darkTheme: darkCodeTheme,
+        },
+    }
 };
 
 module.exports = config;
