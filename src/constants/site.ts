9841d192053936b968eb23f7e6fda56c55472c19
Paillat-dev <me@paillat.dev>
2026-08-21 14:29:44 +0200

    feat: First commit on zensical rework

--- src/constants/site.ts
+++
@@ -1,6 +0,0 @@
-export const site: Record<string, any> = {
-    'name': 'Pycord Guide',
-    'tagline': 'Imagine a place... where you can learn how to add more features to your Pycord bot',
-    'url': 'https://guide.pycord.dev',
-    'orgName': 'Pycord-Development'
-}
